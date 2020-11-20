33c0c4919215e58407b94e979ae282029a4931f1
Victor Alonzo Baldeon Rodriguez <u201413960@upc.edu.pe>
2020-11-20 16:00:18 -0500

    Fix RapidTest

--- src/app/auth/_services/auth.service.ts
+++ src/app/auth/_services/auth.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 //const AUTH_API = 'http://covid-map-2020-back-end.herokuapp.com/api/auth/';
 //const AUTH_API = 'https://covid-map-2020-back-end.herokuapp.com/api/auth/';
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'https://localhost:8080/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
